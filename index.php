53bce03d5121683d0ebbf5bd4523ea3655226b6f
Marina Aye Thidar Tun <marinaayethidartun@gmail.com>
2025-09-06 16:20:31 +0630

    Style Part

--- index.php
+++ index.php
@@ -12,6 +12,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Group - 2, Assignment - 1</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<style>
+        *{
+            margin: 0;
+            padding: 0;
+          
+        }
+        body{
+            padding: 20px 10px;
+        }
+
+        button {
+
+            padding: 0px 3px; 
+
+            border-radius: 6px; 
+          
+        }
+</style>
 </head>
 <body>
 </body>
