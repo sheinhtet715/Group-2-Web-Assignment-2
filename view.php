54c59cda8a505845daf195fb576db5d07110349b
PeterJohn1234 <bhonemyintchain@gmail.com>
2025-09-06 17:51:24 +0700

    view part 4

--- view.php
+++ view.php
@@ -61,3 +61,17 @@ if(isset($_POST['update_button'])){
     }  
 }  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+     <style>
+        *{
+            margin: 0;
+            padding: 0;
+          
+        }
