8dc7fe46b95f5da6b777e56f658b5ef2d99d3a29
sheinhtet715 <sheinhtet715@gmail.com>
2025-09-06 17:06:09 +0630

    student create second part

--- studentscreate.php
+++ studentscreate.php
@@ -19,3 +19,21 @@ $addressError = '';
 
 if (isset($_POST['create_btn'])) {
     $name       = $_POST['name']        ;
+$mobile     = $_POST['mobile']     ;
+$mobile = preg_replace('/\D/', '', $mobile);
+$email      = $_POST['email']      ;
+$gender     = $_POST['gender']    ?? '';
+$department = $_POST['department'] ?? '';
+$address    = $_POST['address']    ;
+if(empty($name)){
+    $nameError = "Name is required";
+}
+if(empty($mobile)){
+    $mobileError = "Mobile no. is required";
+}
+if(empty($email)){
+    $emailError = "Email is required";
+}
+if(empty($gender)){
+    $genderError = "Gender is required";
+}
