22b1219457384d9ddafbb62a175ed6f953e820c9
PeterJohn1234 <bhonemyintchain@gmail.com>
2025-09-06 18:01:27 +0700

    view part 7

--- view.php
+++ view.php
@@ -103,3 +103,33 @@ if(isset($_POST['update_button'])){
 
     </style>
 </head>
+<body>
+
+    <?php if (!isset($post_id_to_update)): ?>
+    <h1>Registered Students</h1><br>
+
+    <table class="table table-bordered">
+        <thead>
+            <tr class="table-secondary">
+                <th>ID</th>
+                <th>Student Name</th>
+                <th>Mobile No.</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Department</th>
+                <th>Address</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+             <?php 
+              $query ="SELECT * FROM registered_students";
+              $students = mysqli_query($db,$query);
+                foreach($students as $student){
+            //  foreach ($students as $student): ?>
+        <tr>
+            <td><?php echo $student['id']; ?></td>
+            <td><?php echo $student['name']; ?></td>
+            <td><?php echo $student['mobile_number']; ?></td>
+            <td><?php echo $student['email']; ?></td>
+            <td><?php echo $student['gender']; ?></td>
